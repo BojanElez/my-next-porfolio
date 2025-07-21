6014cb4e073f3adcb5d56e3176cd7b85e27dcd85
Bojan Elez <bojan.elez@mera.com>
2025-07-21 15:49:38 +0200

    21-07/25: Update Skillset

--- app/[locale]/components/RightColumn/Skills/Skills.tsx
+++ app/[locale]/components/RightColumn/Skills/Skills.tsx
@@ -1,9 +1,33 @@
-import {useTranslations} from 'next-intl';
-import { Subtitle } from '../../common/Subtitle';
+import { useTranslations } from "next-intl";
+import { Subtitle } from "../../common/Subtitle";
 
 export const Skills = () => {
-  const t = useTranslations('Index');
-  const skillsArr = ['HTML', 'CSS', 'Sass/Less','MaterialUI', 'Bootstrap', 'Tailwind', 'JavaScript ES5/ES6+', 'VueJS', 'React', 'Redux', 'React Query', 'NextJS', 'NodeJS', 'Express', 'NestJs', 'SQL', 'Prisma']
+  const t = useTranslations("Index");
+  const skillsArr = [
+    "HTML",
+    "CSS",
+    "Sass/Less",
+    "MaterialUI",
+    "Bootstrap",
+    "Tailwind",
+    "JavaScript ES5/ES6+",
+    "React",
+    "Redux",
+    "React Query",
+    "NextJS",
+    "NodeJS",
+    "Express",
+    "NestJs",
+    "SQL",
+    "Prisma",
+    "AWS",
+    "GraphQL",
+    "REST",
+    "Git",
+    "Docker",
+    "CI/CD",
+    "Agile/Scrum",
+  ];
 
   return (
     <section className="pt-10" id="skills">
@@ -11,13 +35,15 @@ export const Skills = () => {
       <div className="flex flex-wrap">
         {skillsArr.map((skill, index) => {
           return (
-            <div key={index} className="p-2 rounded bg-slate-100 text-fuchsia-400 mr-3 mb-3">
+            <div
+              key={index}
+              className="p-2 rounded bg-slate-100 text-fuchsia-400 mr-3 mb-3"
+            >
               {skill}
             </div>
-          )
+          );
         })}
-        
       </div>
     </section>
-  )
+  );
 };
