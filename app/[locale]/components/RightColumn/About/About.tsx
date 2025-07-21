46ec29b83ddb94527e4f4379292b3e638a80104f
Bojan Elez <bojan.elez@mera.com>
2025-07-21 16:12:47 +0200

    21-07/25: Remove unsed/commented video code

--- app/[locale]/components/RightColumn/About/About.tsx
+++ app/[locale]/components/RightColumn/About/About.tsx
@@ -1,12 +1,10 @@
 import { useTranslations } from 'next-intl';
-import { Video } from '../../Video';
 
 export const About = () => {
   const t = useTranslations('Index');
 
   return (
     <section id="about">
-      {/* <Video /> */}
       <p className="">
         {t("aboutDescription1")}
       </p>
